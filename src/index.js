dd255eb3f11118158ad09f09ff92420f135c3ef1
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-04-16 19:47:06 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    toDoList(e.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function toDoList(text) {
+  let toDoText = document.createElement("p");
+  let submitBtn = document.createElement("button");
+  submitBtn.addEventListener("click", handleDelete);
+  submitBtn.textContent = "x";
+  toDoText.textContent = `${text} `;
+  toDoText.appendChild(submitBtn);
+  document.querySelector("#list").appendChild(toDoText);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
